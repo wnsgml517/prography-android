c2b7ed8a951c032a4e339472e5eb7231db1eb804
wnsgml517 <wnsgml517@naver.com>
2025-02-17 00:58:35 +0900

    [feat/#3] 북마크 recyclerview UI 구현

---
+++ app/src/main/java/com/android/prography/ui/home/BookMarkImageAdapter.kt
@@ -0,0 +1,30 @@
+package com.android.prography.ui.home
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.android.prography.R
+import com.bumptech.glide.Glide
+
+class BookMarkImageAdapter(private val itemList: List<ImageItem>) :
+    RecyclerView.Adapter<BookMarkImageAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val imageView: ImageView = view.findViewById(R.id.iv_bookmark_image)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_bookmark_image, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = itemList[position]
+        Glide.with(holder.itemView.context).load(item.imageUrl).into(holder.imageView)
+    }
+
+    override fun getItemCount(): Int = itemList.size
+}
--- app/src/main/java/com/android/prography/ui/home/HomeFragment.kt
+++ app/src/main/java/com/android/prography/ui/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.android.prography.databinding.FragmentHomeBinding
 
@@ -36,6 +37,7 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initRecentImage()
+        initBookmarkImage()
     }
 
     override fun onDestroyView() {
@@ -43,15 +45,46 @@ class HomeFragment : Fragment() {
         _binding = null
     }
 
-    private fun initRecentImage(){
+    private fun initRecentImage() {
         val itemList = listOf(
-            ImageItem("https://source.unsplash.com/random/300x200", "titletitletitle", "타이틀은 최대 2줄까지"),
-            ImageItem("https://source.unsplash.com/random/301x350", "titletitletitle", "타이틀은 최대 2줄까지"),
-            ImageItem("https://source.unsplash.com/random/302x250", "titletitletitle", "타이틀은 최대 2줄까지"),
-            ImageItem("https://source.unsplash.com/random/303x400", "titletitletitle", "타이틀은 최대 2줄까지")
+            ImageItem(
+                "https://source.unsplash.com/random/300x200",
+                "titletitletitle",
+                "타이틀은 최대 2줄까지"
+            ),
+            ImageItem(
+                "https://source.unsplash.com/random/301x350",
+                "titletitletitle",
+                "타이틀은 최대 2줄까지"
+            ),
+            ImageItem(
+                "https://source.unsplash.com/random/302x250",
+                "titletitletitle",
+                "타이틀은 최대 2줄까지"
+            ),
+            ImageItem(
+                "https://source.unsplash.com/random/303x400",
+                "titletitletitle",
+                "타이틀은 최대 2줄까지"
+            )
         )
 
-        binding.rvRecentImage.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        binding.rvRecentImage.layoutManager =
+            StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
         binding.rvRecentImage.adapter = RecentImageAdapter(itemList)
+    }
+
+
+    private fun initBookmarkImage() {
+        val itemList = listOf(
+            ImageItem("https://source.unsplash.com/random/200x200", "이미지1","북마크이미지입니다."),
+            ImageItem("https://source.unsplash.com/random/201x200", "이미지2","북마크이미지입니다."),
+            ImageItem("https://source.unsplash.com/random/202x200", "이미지3","북마크이미지입니다."),
+            ImageItem("https://source.unsplash.com/random/203x200", "이미지4","북마크이미지입니다.")
+        )
+
+        binding.rvBookmark.layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+
+        binding.rvBookmark.adapter = BookMarkImageAdapter(itemList)
     }
 }
