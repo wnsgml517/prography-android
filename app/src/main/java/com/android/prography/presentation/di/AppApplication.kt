8dc6d520f185e6b2ad20e7f75505bf1960c12701
wnsgml517 <wnsgml517@naver.com>
2025-02-19 01:58:01 +0900

    [feat/#1] hilt 설정

---
+++ app/src/main/java/com/android/prography/presentation/di/AppApplication.kt
@@ -0,0 +1,14 @@
+package com.android.prography.presentation.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
+
+@HiltAndroidApp
+class AppApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
